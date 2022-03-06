c258d0430eaaf9f09152b8e931c89b15e785f1cd
07HaleemaZehera <hzehera07@gmail.com>
2022-03-06 13:57:55 +0530

    Props
    Props

---
+++ d_03_State,props,hooks/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { Counter } from './components/Counter/Counter';
+// import{Counter} from "./components/Counter/Counter";
+
+
+
+function App() {
+  return (
+    <>
+{/* <Counter value={10}  isMarried={true}/>
+<Counter value={50}/> */}
+<Counter value={0}/>
+{/* <Counter/> */}
+
+
+
+    </>
+  );
+}
+export default App;
---
+++ d_03_State,props,hooks/src/components/Counter/Counter.jsx
@@ -0,0 +1,37 @@
+
+import{useState} from "react";
+import './CounterCss/CounterCss.css';
+export function Counter(props){
+    // console.log("Counter received props:",props)
+    const [counter,setCounter]=useState(0);
+    const handleChange=(value)=>{
+        setCounter(counter+2+value)
+    }
+    const handleDouble=(value)=>{
+        setCounter(counter*2*value)
+    }
+
+    return  <h3>
+        <div className="heading">
+            <h3 >
+                Counter:{counter}
+            </h3>
+            <button className="name"onClick={()=>{handleChange(1)
+               
+            }}>Increment
+            </button><br>
+            </br><br></br>
+            <button className="name"onClick={()=>{handleChange(-1)
+                // setCounter(counter-1)
+            }}>Decrement
+                </button>
+                <br></br>
+                <br></br>
+                <button className="name" onDoubleClick={()=>handleDouble(2)}>
+                    Double Click
+                    {/* alert("The answer for double clicked Number is") */}
+                </button>
+        </div>
+       
+     </h3>
+}
